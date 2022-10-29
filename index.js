c513aad10e5f256f32af9e74ad7b415d233185bd
JennyRJ <jeninepaula51@gmail.com>
2022-10-29 12:43:12 +0300

    Random numbers,validate score

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
 const questionE1 = document.getElementById("question");
-const inputE1 = document.getElementById("input");
 const formE1 = document.getElementById("form");
-
-let score = 0;
+const inputE1 = document.getElementById("input");
+const scoreE1 = document.getElementById("score");
 
 questionE1.innerText = `What is ${num1} multiply by ${num2}?`;
+let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+    score = 0;
+}
+
+scoreE1.innerText = `score :${score}`;
 
 const correctAns = num1 * num2;
 
-formE1.addEventListener("Submit", () => {
+formE1.addEventListener("submit", () => {
     const userAns = +inputE1.value;
+
     if (userAns === correctAns) {
         score++;
+        updateScore();
     } else {
-        scoree--;
-        updateLocalStorage();
+        score--;
+        updateScore();
     }
 });
 
-function updateLocalStorage() {
-    localStorage.setItem("score", Json.stringify(score));
+function updateScore() {
+    localStorage.setItem("score", JSON.stringify(score));
 }
